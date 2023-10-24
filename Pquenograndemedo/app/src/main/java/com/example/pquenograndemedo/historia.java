90a7ba410dc9919405d08f377caecad22dd64ff5
danilodhog <danilo.garcia16@etec.sp.gov.br>
2023-10-24 18:59:35 -0300

    versão 1.2.3

--- Pquenograndemedo/app/src/main/java/com/example/pquenograndemedo/historia.java
+++ Pquenograndemedo/app/src/main/java/com/example/pquenograndemedo/historia.java
@@ -10,10 +10,20 @@ import android.widget.Button;
 
 public class historia extends AppCompatActivity {
 
+
+    Button buttonvtr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historia);
+
+        Button voltar = findViewById(R.id.buttonvtr);
+        voltar.setOnClickListener(view -> {
+            Intent intent;
+            intent = new Intent(historia.this, MainActivity.class);
+            startActivity(intent);
+
+        });
 
     }
     }
