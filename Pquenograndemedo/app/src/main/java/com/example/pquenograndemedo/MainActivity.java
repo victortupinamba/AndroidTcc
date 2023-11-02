dc477563128e9ebc7deaf77a2d0b206ca9fec73a
danilodhog <danilo.garcia16@etec.sp.gov.br>
2023-11-02 17:18:16 -0300

    1.3.0
    versão final (faltam ajustes de design, apenas)

--- Pquenograndemedo/app/src/main/java/com/example/pquenograndemedo/MainActivity.java
+++ Pquenograndemedo/app/src/main/java/com/example/pquenograndemedo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.pquenograndemedo;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button hist = findViewById(R.id.btnhist);
+        ImageButton hist = findViewById(R.id.btnhist);
         hist.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, historia.class);
             startActivity(intent);
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button perso = findViewById(R.id.btnperso);
+        ImageButton perso = findViewById(R.id.btnperso);
         perso.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, personagem.class);
             startActivity(intent);
@@ -29,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        Button EasterEgg = findViewById(R.id.btnEasteregg);
+        ImageButton EasterEgg = findViewById(R.id.btnEasteregg);
         EasterEgg.setOnClickListener(view -> {
             Intent intent;
             intent = new Intent(MainActivity.this, easteregg.class);
@@ -37,12 +41,26 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        Button RedesSociais = findViewById(R.id.btnRedesSociais);
+        ImageButton RedesSociais = findViewById(R.id.btnRedesSociais);
         RedesSociais.setOnClickListener(view -> {
             Intent intent;
             intent = new Intent(MainActivity.this, redessocias.class);
             startActivity(intent);
 
+        });
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton btnSpotify = findViewById(R.id.imagebtnVoltar);
+        btnSpotify.setOnClickListener(v -> {
+            String url = "https://open.spotify.com/playlist/3dhi5IcjKffrhhCpM7rv2S?si=440bb814dc1e42eb&pt=029efe8907bd076fdd0361f4cf525c18";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
+        });
+
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageButton btnThreads = findViewById(R.id.imageButton);
+        btnThreads.setOnClickListener(v -> {
+            String url = "https://www.linkedin.com/in/sirius-entertainment-5824b5270?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app";
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+            startActivity(intent);
         });
     }
 }
--- Pquenograndemedo/app/src/main/java/com/example/pquenograndemedo/personagem.java
+++ Pquenograndemedo/app/src/main/java/com/example/pquenograndemedo/personagem.java
@@ -2,7 +2,9 @@ package com.example.pquenograndemedo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class personagem extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class personagem extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_personagem);
+
+        Button voltar = findViewById(R.id.buttonvol);
+        voltar.setOnClickListener(view -> {
+            Intent intent;
+            intent = new Intent(personagem.this, MainActivity.class);
+            startActivity(intent);
+
+        });
 
 
     }
